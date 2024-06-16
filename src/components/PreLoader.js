fb8dc076dc958b735478bb03a7da2b7aa813332f
Chris Xiong <xiongchris1@gmail.com>
2024-06-16 00:47:24 -0500

    deleted gsap animations

--- src/components/PreLoader.js
+++ src/components/PreLoader.js
@@ -1,33 +1,27 @@
-import React, { useEffect } from "react";
-import { preLoaderAnim } from "../animations";
-
 const PreLoader = () => {
-  useEffect(() => {
-    preLoaderAnim();
-  }, []);
-
   return (
     <div
       className="preloader fixed inset-0 flex z-55 items-center
       overflow-hidden justify-center bg-black"
     >
       <div className="landing__top flex flex-col items-center justify-center">
-        <span className="sub text-white text-5xl font-bold">Deez</span>
+        <span className="sub text-white animatetext-5xl font-bold">
+          Loading...
+        </span>
       </div>
       <div className="landing__main flex flex-col items-center justify-center mt-4">
         <span
           className=" text-5xl items-center justify-between 
-        font-bold text-white animate-ping overflow-hidden"
+        font-bold text-white overflow-hidden"
         >
-          Nuts
+          Chris Xiong
         </span>
       </div>
       <div className="shapes flex flex-wrap justify-center mt-4">
-        <div className="shape main-circle w-16 bg-red-500 rounded-full m-2"></div>
-        <div className="shape shape-1 w-1 h-12 bg-green-500 rounded-full m-2"></div>
+        <div className="shape main-circle w-16 h-12 bg-white rounded-full m-2"></div>
+        <div className="shape shape-1 w-16 h-12 bg-blue rounded-full m-2"></div>
       </div>
     </div>
   );
 };
-
 export default PreLoader;
